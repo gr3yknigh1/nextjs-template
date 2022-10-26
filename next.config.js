ac802767817cc95d4b939358a0db5b7b5f35391a
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-10-26 23:13:39 +0300

    Remove placeholder.com from images domain in next config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
-  images: {
-    domains: ["via.placeholder.com"],
-    loader: "imgix",
-    path: "https://via.placeholder.com/",
-  },
 };
 
 module.exports = nextConfig;
